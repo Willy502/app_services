9e44e5c593f27823a87ff56d6d0b611428e2f548
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-06-12 09:42:30 -0600

    Splash and login

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:app_services/src/pages/login_page.dart';
+import 'package:app_services/src/pages/splash_page.dart';
+
+Map<String, WidgetBuilder> getApplicationRoutes() {
+  return <String, WidgetBuilder> {
+    'splash' : (context) => SplashPage(),
+    'login' : (context) => LoginPage()
+  };
+}
